cf88cf22e2c7133c999d9d45ab6cc8215d22aff6
Furesoft <filmee24@gmail.com>
2019-11-14 08:15:59 +0100

    added updater

--- DedSecDns-Client/DedSecDns-Client/SplashScreen.Designer.cs
+++ DedSecDns-Client/DedSecDns-Client/SplashScreen.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DedSecDns_Client
 {
-    partial class Form1
+    partial class SplashScreen
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1243, 539);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
