28e5f47d0448fb9bbfd8f04625c2a7ee4efa8f1a
Furesoft <filmee24@gmail.com>
2019-11-14 20:55:00 +0100

    added user model

---
+++ DedSecDns-Client/DedSecDns-Client/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DedSecDns_Client.Models
+{
+    public class User
+    {
+        public string email { get; set; }
+
+        public bool includeEmailInRedirect { get; set; }
+        public bool mark_email_as_verified { get; set; }
+    }
+}
