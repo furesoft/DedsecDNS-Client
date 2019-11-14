42c3376d351a53d6ff93900a0a320774f92c8a2d
Furesoft <filmee24@gmail.com>
2019-11-14 20:55:12 +0100

    added forgot password

---
+++ DedSecDns-Client/DedSecDns-Client/Controllers/ForgotPasswordController.cs
@@ -0,0 +1,17 @@
+﻿using DedSecDns_Client.Core.MVC;
+using DedSecDns_Client.Views;
+
+namespace DedSecDns_Client.Controllers
+{
+    internal class ForgotPasswordController : Controller
+    {
+        public override IView View => _view ?? new ForgotPasswordView();
+
+        public override bool Loadable()
+        {
+            return true;
+        }
+
+        private IView _view;
+    }
+}
