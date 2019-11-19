4ef07f7c9af796ec7b1b17045896926a69d04aaa
Furesoft <filmee24@gmail.com>
2019-11-19 16:21:09 +0100

    ssome test ui

--- DedSecDns-Client/DedSecDns-Client/Pages/ManageDomainsPage.Designer.cs
+++ DedSecDns-Client/DedSecDns-Client/Pages/ManageDomainsPage.Designer.cs
@@ -28,12 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.xuiBanner1 = new XanderUI.XUIBanner();
+            this.xuiCard1 = new XanderUI.XUICard();
             this.SuspendLayout();
+            // 
+            // xuiBanner1
+            // 
+            this.xuiBanner1.BackColor = System.Drawing.Color.Transparent;
+            this.xuiBanner1.BannerColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(71)))), ((int)(((byte)(89)))));
+            this.xuiBanner1.BorderColor = System.Drawing.Color.White;
+            this.xuiBanner1.ForeColor = System.Drawing.Color.White;
+            this.xuiBanner1.Location = new System.Drawing.Point(531, 104);
+            this.xuiBanner1.Name = "xuiBanner1";
+            this.xuiBanner1.Size = new System.Drawing.Size(100, 20);
+            this.xuiBanner1.TabIndex = 0;
+            this.xuiBanner1.Text = "xuiBanner1";
+            // 
+            // xuiCard1
+            // 
+            this.xuiCard1.BackColor = System.Drawing.Color.Transparent;
+            this.xuiCard1.Color1 = System.Drawing.Color.DodgerBlue;
+            this.xuiCard1.Color2 = System.Drawing.Color.LimeGreen;
+            this.xuiCard1.ForeColor = System.Drawing.Color.White;
+            this.xuiCard1.Location = new System.Drawing.Point(95, 90);
+            this.xuiCard1.Name = "xuiCard1";
+            this.xuiCard1.Size = new System.Drawing.Size(320, 170);
+            this.xuiCard1.TabIndex = 1;
+            this.xuiCard1.Text = "xuiCard1";
+            this.xuiCard1.Text1 = "Savings Card";
+            this.xuiCard1.Text2 = "1234 5678 9101 1121";
+            this.xuiCard1.Text3 = "Exp: 01/02 - 03/04";
             // 
             // ManageDomainsPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.xuiCard1);
+            this.Controls.Add(this.xuiBanner1);
             this.Name = "ManageDomainsPage";
             this.Size = new System.Drawing.Size(908, 499);
             this.ResumeLayout(false);
@@ -41,5 +72,8 @@
         }
 
         #endregion
+
+        private XanderUI.XUIBanner xuiBanner1;
+        private XanderUI.XUICard xuiCard1;
     }
 }
--- DedSecDns-Client/DedSecDns-Client/Pages/ManageTldPage.Designer.cs
+++ DedSecDns-Client/DedSecDns-Client/Pages/ManageTldPage.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManageTldPage));
+            this.xuiGauge1 = new XanderUI.XUIGauge();
+            this.xuiLineGraph1 = new XanderUI.XUILineGraph();
             this.SuspendLayout();
+            // 
+            // xuiGauge1
+            // 
+            this.xuiGauge1.DialColor = System.Drawing.Color.Gray;
+            this.xuiGauge1.DialThickness = 5;
+            this.xuiGauge1.FilledColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(162)))), ((int)(((byte)(250)))));
+            this.xuiGauge1.GaugeStyle = XanderUI.XUIGauge.Style.Material;
+            this.xuiGauge1.Location = new System.Drawing.Point(496, 163);
+            this.xuiGauge1.Name = "xuiGauge1";
+            this.xuiGauge1.Percentage = 75;
+            this.xuiGauge1.Size = new System.Drawing.Size(140, 70);
+            this.xuiGauge1.TabIndex = 0;
+            this.xuiGauge1.Text = "xuiGauge1";
+            this.xuiGauge1.Thickness = 8;
+            this.xuiGauge1.UnfilledColor = System.Drawing.Color.Gray;
+            // 
+            // xuiLineGraph1
+            // 
+            this.xuiLineGraph1.BackGroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(217)))), ((int)(((byte)(174)))));
+            this.xuiLineGraph1.BelowLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(202)))), ((int)(((byte)(142)))));
+            this.xuiLineGraph1.BorderColor = System.Drawing.Color.White;
+            this.xuiLineGraph1.GraphStyle = XanderUI.XUILineGraph.Style.Material;
+            this.xuiLineGraph1.GraphTitle = "XUI LineGraph";
+            this.xuiLineGraph1.GraphTitleColor = System.Drawing.Color.Gray;
+            this.xuiLineGraph1.Items = ((System.Collections.Generic.List<int>)(resources.GetObject("xuiLineGraph1.Items")));
+            this.xuiLineGraph1.LineColor = System.Drawing.Color.White;
+            this.xuiLineGraph1.Location = new System.Drawing.Point(223, 221);
+            this.xuiLineGraph1.Name = "xuiLineGraph1";
+            this.xuiLineGraph1.PointSize = 7;
+            this.xuiLineGraph1.ShowBorder = false;
+            this.xuiLineGraph1.ShowPoints = true;
+            this.xuiLineGraph1.ShowTitle = false;
+            this.xuiLineGraph1.ShowVerticalLines = false;
+            this.xuiLineGraph1.Size = new System.Drawing.Size(200, 100);
+            this.xuiLineGraph1.TabIndex = 1;
+            this.xuiLineGraph1.Text = "xuiLineGraph1";
+            this.xuiLineGraph1.TitleAlignment = System.Drawing.StringAlignment.Near;
+            this.xuiLineGraph1.VerticalLineColor = System.Drawing.Color.DimGray;
             // 
             // ManageTldPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.xuiLineGraph1);
+            this.Controls.Add(this.xuiGauge1);
             this.Name = "ManageTldPage";
             this.Size = new System.Drawing.Size(896, 418);
             this.ResumeLayout(false);
@@ -41,5 +84,8 @@
         }
 
         #endregion
+
+        private XanderUI.XUIGauge xuiGauge1;
+        private XanderUI.XUILineGraph xuiLineGraph1;
     }
 }
