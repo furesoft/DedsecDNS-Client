bd2c2a46dd568a6f152a53598cc2162f38510394
Furesoft <filmee24@gmail.com>
2019-11-20 11:43:13 +0100

    added dnsservice base

---
+++ DedSecDns-Client/DedSecDns-Client/DnsService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DedSecDns_Client
+{
+    public static class DnsService
+    {
+        public static IEnumerable<string> GetAllDomains()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<string> GetAllTldNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object GetDomain(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object GetTld(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
