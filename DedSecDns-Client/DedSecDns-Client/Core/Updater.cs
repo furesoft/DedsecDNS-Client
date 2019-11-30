06b0aef64b4d0729f66facfba7a5ee9b3b623a5e
Furesoft <filmee24@gmail.com>
2019-11-30 11:00:43 +0100

    updater reconfigured

--- DedSecDns-Client/DedSecDns-Client/Core/Updater.cs
+++ DedSecDns-Client/DedSecDns-Client/Core/Updater.cs
@@ -1,6 +1,7 @@
 ﻿using nUpdate.Updating;
 using System;
 using System.Globalization;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace DedSecDns_Client.Core
@@ -9,7 +10,10 @@ namespace DedSecDns_Client.Core
     {
         public static void InstallIfAvailable()
         {
-            var manager = new UpdateManager(new Uri("http://furesoft.ml/updates/dns-client/updates.json"), "<RSAKeyValue><Modulus>6TWTaOx+6MebUYg4MYPtpj0iN/6+0KP7RdQXaWyhjaHV4p71q2+Qk6NenyRydqy1bxakdma8WahyBS6D25lEo6Lro+pKXea7jtF3mglVKzoRqB6InHQCxC2kFSAm9tlDAwxxU0T9ZtgUM4zm35czzV9RwNutU+ShWjW0xqKCep6VRMM/JE179HnTmMpgfD0ZnbjPMHMrhtMY0Um1Sx+kXHUHcwe2eQfIGanpvDSXI/Cti4ztVrGXPtyDSHrGiMJz/Ay0GzkcYzQgsTCXDI/f73fh/4mNRRD/A+8L+XyCDufq2iWHNspDXKa3it5r69Vdh/kRI/irvOcx6tmukmW+SDBmCpqOmGFITwcTN88qLH07z7MpLbWXOA9V+fti/P6Pqg9qwWdDu8O5fxG0C+VCVK304JLhNOO5MeYiYkAj1yz5tRrY9qDUZ8sAAdk8UGnCscPaMyWEF6CTXClJQlhL/NbZ6U9PgM8W+tKpQ6KvEJi2DcR1mLAy3c29MBlkrPtBnHpZt30JVZzwXJDBMBR476wWiDWrPOan2zTa291NAfLHTff2HGC/HiPoAEtymtNdNQ3fJSPtU2T437PiS5ykchYIBJfvh18jvYX44CDKtImrTheByq0rUvDvgI9EXRJriZaU0g6cQ3vXWVtnpirk6ST7ZPw/biEq1oLWUmXlP9ZwzV5dm41oRIF7VN7rXSS3wEimHcu7147ocCKOxXm4+gS0kZhAYakrhJQLoXR8N1BEWdaJM+mtcUf+N4UFJdko+n4wjqbMr+a789ONF6IcCom8VoIb2+tXmZtIR0+CgkwJulGRMS/rmd5MJ/M25GSxHQoCX9BCi3SgQy/BPU4rzMXHfTztl49zWpdDwvppX6YzMh4DxAXu/x8/UhDPNVB9nJpl03XVVlAtupfO+Mk/zruVAZNxfQobEbTRiLNLZdbTCHmwbLy5gFnhHX6IwN+9d6nhWQXaWo32JkWbaEmB8tSMDyfVJQcUKKTSwzOa2nWnNtHS5BYV9wFXo3vPY2reyEoBtVsMDKv2p2WWgDL9jtJVAFOc6Ll77CotUIgSixIafhG4cScJorDlvxlnEDCN7WIp5v3BpUS3aJxekSJOzMSnHtl7NlL0Hu9FaDSmfbOtZhMUk/JIazlRRy++YjrAEoTplRzU26rvHaNiTmRHg6pZR7Ngtj6OdUkpS83HwOB/3JQuNfycyuaXoMyibv2ytqi7IE2uoFHqRTp7H4+ryoNlH+F+HwJ5roUG7nPNChuTwvFAYUf9bVfV6xag6KO2WGtFK3jCWjEy0bMMVHvrt8Wn+4+HkiNETBAUbbWm4f++xtNvQL2s80DGyv1ePi1fCFBNiQFk++sutAovGYL5eQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>", new CultureInfo("de-DE"));
+            var currentVersion = new UpdateVersion(Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+            var manager = new UpdateManager(new Uri("http://furesoft.bplaced.net/updates/dns-client/updates.json"), "<RSAKeyValue><Modulus>taqq8+iZppH4sm8L49YHAWBf/l5HkJik9jl452i5zegfUYYpNfAAyWEmZlbFROGp/JymFU87+OAnLNT87IuT2QI/zQ8mUPJCdSK7nE1dp2RpLFvloJOuUER8rjJnS/R6ZfgrklaprBRpneKaW84h5FaczvFUPlvZYHtg1+bdD3bVWj81GjSEjWOaY8+gr0DEMhXCofvtgUUShRSeRivTAtVPUwPQPxn4IjsiCaBtyW4IaArdSFDGiKIW861P+SVzvEbZec3vHP0WvS/OT8mB9V6Cj7euCD5Bdq7mbTkmPW+LLAAretbbT82DdDInMBWBdA7An/SSTVoJNXirOE4bYQp5ZkmvE2lzMs4yUiXqFlWc2ukx/L20IGCMA5p7+TRYmw/BRFoMUntmMbVXh2m9m/4I0hafTXMS1+t6Zq3pBmeiBBHd7+vtYH3WU/a4Y45FSZqr/sVErLy7pPWbmKCSYY7IyKWMb3npu9Y9A8SNwzVqXy5HETq0xFJNbZ3+lpocj7fXnMLv/7H5QEood3wMFv1wet38BOX6mwvTdjWuwlUCsRNWNIghP1yvdW0mL1pL15l5lroycQDz+JT2Mld9YaLIQ5NbxF24HVW4cq5aFXKoOh8V6rDUvhYTEZz0zGpthpzulvtYGoYmnQrCfLSWiGxa4BkuzGpaT1D6qX1dX2irp66OYoj3qJOsIU94jP6va8/5wxp2+7pPEBw03/b63M9079Rijf/XBw6/CFNxRU9jdYuPzXtgfTXNXuKvuTTx0cDbHPQ3BHuIjkPBQTp8tt8FsT895cGfwcISEyZWqlTug+feo5+8g4yem5qDIc/2htPsJOb+ehjRwFsCFPnbZQgJePn5UConQCuZX09bXRGhZt1H5pqF8S0wZBBp6s4ePOnrNzqj6pglj6q9JZFFKVIMgm63waNlKCF/14YY4ZvCtB+j2FmVZ/jrWgLZV1mBxX1uk98KwslZIG4sHoalNaQxlZeVSJ1mDmyu84X8tsRfe514gdkNDcTI2MDCLIxKnfeyGrHqydNkHlqL7zDNRLIh8iapEz4Fx5pXFIZ6HNbnyPKLhiIUuFgP1Fr7oCF1GLhrIbDQEutc71USzDkGlEskONd0hAAyKVq7NKP4KyI0bSurw7UawD6/rk5f2e96VDgC4fcpiZ6DTlhfcnl+OxOWtRlVbRvzvn9IwzwpiGtrbtnnVwKh4cjV/FJJbDG9Ax3g3/NrzLT99KP0scJqgbYP0wsecqAmezSxm6RRK2L6Y1H0rop1LVtD43MjoSfE1rocoKMms5+ZUffObHQB3zAUxg+3Igd4sO4TRGCzs/9eX0O4D/8YL2B9tj2ifAFVcaOFcLkBRG0sYD02eUI72Q==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>", new CultureInfo("en"), currentVersion);
+            manager.RunInstallerAsAdmin = false;
 
             if (manager.SearchForUpdates())
             {
