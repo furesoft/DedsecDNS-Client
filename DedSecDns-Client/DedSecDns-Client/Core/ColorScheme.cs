af6a80c19ee64f0ad90cd30fd1af2a473b746c14
Furesoft <filmee24@gmail.com>
2020-01-02 11:02:41 +0100

    color scheme fix

--- DedSecDns-Client/DedSecDns-Client/Core/ColorScheme.cs
+++ DedSecDns-Client/DedSecDns-Client/Core/ColorScheme.cs
@@ -1,10 +1,12 @@
 ﻿using Dedsec.Parsing;
 using DedSecDns_Client.Core.Styling.Parser;
 using DedSecDns_Client.Properties;
+using Loyc.Syntax;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using XanderUI;
 
 namespace DedSecDns_Client.Core
 {
@@ -12,13 +14,23 @@ namespace DedSecDns_Client.Core
     {
         public static ColorScheme Default = Load("default", Resources.default_colors);
         public string Name { get; set; }
-        public IEnumerable<CssParserDeclaration> Style { get; set; }
+        public LNode Style { get; set; }
 
         public static void Apply(ColorScheme scheme, Control c)
         {
-            c.BackColor = scheme.GetColor("background");
-
-            foreach (var btn in c.Controls.OfType<XanderUI.XUIButton>())
+            if (c is Form)
+            {
+                c.BackColor = Color.FromArgb(255, scheme.GetColor("background"));
+            }
+            else
+            {
+                c.BackColor = scheme.GetColor("background");
+            }
+            foreach (var btn in c.Controls.OfType<XUIWidgetPanel>())
+            {
+                btn.BackColor = scheme.GetColor("dark");
+            }
+            foreach (var btn in c.Controls.OfType<XUIButton>())
             {
                 btn.BackColor = scheme.GetColor("dark");
             }
@@ -26,20 +38,22 @@ namespace DedSecDns_Client.Core
 
         public static ColorScheme Load(string name, string source)
         {
-            var hex = new ColorSchemeParser().ParseProperty("background: hls(1, 2, 3);");
-
-            var style = new CssParser().ParseAll(source);
+            var p = new ColorSchemeParser();
+            var style = p.Parse(source);
 
             return new ColorScheme { Name = name, Style = style };
         }
 
         public Color GetColor(string key)
         {
-            foreach (var decl in Style)
+            foreach (var decl in Style.Args)
             {
-                if (decl.Property == key)
+                var k = decl.Args.First();
+                var v = decl.Args[1];
+
+                if (k.Name.Name == key)
                 {
-                    return new Styling.ColorConverter().Convert(decl.Value);
+                    return (Color)v.Value;
                 }
             }
 
